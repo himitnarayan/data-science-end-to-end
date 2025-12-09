b00a8c44806931a96b3254cf7b0113eeb5d6162e
himitnarayan <himitnarayan31@gmail.com>
2025-12-09 23:42:46 +0530

    change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HYPEN_E_DOT = "-e ."
 
 def get_requirements(file_path: str) -> List[str]:
     requirements=[]
-    with open(file_path, 'r') as file:
+    with open(file_path) as file:
         requirements = file.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
 
